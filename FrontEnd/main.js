218bcf8ad2bb28c9ee3fcb1f6fbb7f871b64a492
Benjamin W <71712365+OnixTakeSama@users.noreply.github.com>
2023-10-20 16:45:06 +0200

    Adding new ESLint rule

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,22 +1,19 @@
-const dataWorks = fetch("http://localhost:5678/api/works");
-const gallery = document.querySelector(".gallery");
+const dataWorks = fetch('http://localhost:5678/api/works');
+const gallery = document.querySelector('.gallery');
 
-dataWorks.then((response) => {
-    return response.json();
-}
-).then((data) => {
-    for (let i = 0; i < data.length; i++) {
-        const figure = document.createElement("figure");
-        gallery.appendChild(figure);
+dataWorks.then((response) => response.json()).then((data) => {
+  for (let i = 0; i < data.length; i++) {
+    const figure = document.createElement('figure');
+    gallery.appendChild(figure);
 
-        const img = document.createElement("img");
-        img.src = data[i].imageUrl;
-        img.alt = data[i].title;
-        img.crossOrigin = "anonymous";
-        figure.appendChild(img);
+    const img = document.createElement('img');
+    img.src = data[i].imageUrl;
+    img.alt = data[i].title;
+    img.crossOrigin = 'anonymous';
+    figure.appendChild(img);
 
-        const figCaption = document.createElement("figcaption");
-        figCaption.textContent = data[i].title;
-        figure.appendChild(figCaption);
-    }
+    const figCaption = document.createElement('figcaption');
+    figCaption.textContent = data[i].title;
+    figure.appendChild(figCaption);
+  }
 });
