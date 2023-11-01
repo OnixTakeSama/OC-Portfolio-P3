591ef084115c0aa2b03f4a4d35f71a3409f2501c
Benjamin W <71712365+OnixTakeSama@users.noreply.github.com>
2023-11-01 20:29:43 +0100

    feat(modal): Adding modal window
    Adding modal window
    Displaying works in the modal window

---
+++ FrontEnd/modal.js
@@ -0,0 +1,39 @@
+import { loggedButton, modalContainer, modalGallery, modalTriggers} from "./scripts/domLinker.js";
+import { getWorks, getCategories } from "./scripts/api.js";
+
+modalTriggers.forEach(trigger => trigger.addEventListener('click', toggleModal));
+
+function toggleModal() {
+    modalContainer.classList.toggle('active');
+    displayModalWorks();
+}
+
+function createElement(modalWork) {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    img.src = modalWork.imageUrl;
+    img.alt = modalWork.title;
+    img.crossOrigin = "anonymous";
+    figure.appendChild(img);
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "<i class='fa-solid fa-trash-can'></i>";
+    deleteButton.classList.add("delete-button");
+    figure.appendChild(deleteButton);
+    // deleteButton.addEventListener("click", () => {
+    //     const id = modalWork.id;
+    //     deleteWork(id);
+    // });
+    return figure;
+}
+
+const displayModalWorks = () =>
+    getWorks()
+        .then((data) => {
+                modalGallery.innerHTML = ''
+                data.forEach((work) => {
+                        console.log(work);
+                        const element = createElement(work);
+                        modalGallery.appendChild(element);
+                });
+            });
+
--- FrontEnd/scripts/domLinker.js
+++ FrontEnd/scripts/domLinker.js
@@ -1,11 +1,16 @@
 /** Page index.html **/
 export const gallery = document.querySelector(".gallery");
-export const logoutLink = document.querySelector(".logout");
 export const loginLink = document.getElementById("login-a");
 export const filters = document.querySelector(".filters");
 export const navLinks = document.querySelectorAll("nav a");
+
+export const logoutLink = document.querySelector(".logout");
 export const loggedBanner = document.querySelector(".logged-banner");
 export const loggedButton = document.querySelector(".logged-button");
+
+export const modalContainer = document.querySelector(".modal-container");
+export const modalTriggers = document.querySelectorAll(".modal-trigger");
+export const modalGallery = document.querySelector(".modal-gallery");
 
 /** Page logoin.html **/
 export const loginForm = document.querySelector('#login-form');
