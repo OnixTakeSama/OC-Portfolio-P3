7d3c34d405972ff29216de0b2f3799fc97d87c6d
Benjamin W <71712365+OnixTakeSama@users.noreply.github.com>
2023-11-09 21:15:49 +0100

    feat(addWorkModal): Modifying modal style
    Modifying modal style (need to fix input file button)
    Adding some functions to display the correct modal
    Adding delete work / add workk functions (need to test them)

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,17 +1,53 @@
-import { addWorkButton, modalContainer, modalGallery, modalTriggers, modalContentGallery, modalContentAddWork} from "./scripts/domLinker.js";
+import { addWorkButton, modalContainer, modalGallery, modalTriggers, modalContentGallery, modalContentAddWork, categorySelector, addPhotoFile, previewPhoto, blankPhoto, addPhotoLabel, photoSize, returnButton} from "./scripts/domLinker.js";
 import { getWorks, getCategories } from "./scripts/api.js";
 
 modalTriggers.forEach(trigger => trigger.addEventListener('click', toggleModal));
-addWorkButton.addEventListener('click', displayWorkForm);
+addWorkButton.addEventListener('click', displayAddWorkForm);
+returnButton.addEventListener('click', displayModalGallery);
 
 function toggleModal() {
     modalContainer.classList.toggle('active');
+    modalContentGallery.classList.remove('hidden');
+    modalContentAddWork.classList.add('hidden');
     displayModalWorks();
 }
 
-function displayWorkForm() {
+function displayModalGallery() {
+    modalContentGallery.classList.remove('hidden');
+    modalContentAddWork.classList.add('hidden');
+    returnButton.classList.add('hidden');
+    displayModalWorks();
+}
+
+function displayAddWorkForm() {
     modalContentGallery.classList.add('hidden');
     modalContentAddWork.classList.remove('hidden');
+    returnButton.classList.remove('hidden');
+    displayCategories();
+}
+
+addPhotoFile.onchange = evt => {
+    const [file] = addPhotoFile.files
+    if (file) {
+        previewPhoto.src = URL.createObjectURL(file)
+        blankPhoto.classList.add('hidden');
+        previewPhoto.classList.remove('hidden');
+        addPhotoFile.classList.add('hidden');
+        addPhotoLabel.classList.add('hidden');
+        photoSize.classList.add('hidden');
+    }
+  }
+
+function displayCategories() {
+    getCategories()
+        .then((data) => {
+            data.forEach((category) => {
+                const option = document.createElement("option");
+                option.value = category.id;
+                option.text = category.name;
+                categorySelector.appendChild(option);
+            });
+        });
 }
 
 function createElement(modalWork) {
@@ -43,3 +79,40 @@ const displayModalWorks = () =>
                 });
             });
 
+function deleteWork(id) {
+    fetch(`http://localhost:5678/api/works/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((response) => response.json())
+        .then(() => {
+            displayModalWorks();
+        });
+}
+
+function addWork() {
+    const imageUrl = document.querySelector("#photo-url").value;
+    const title = document.querySelector("#photo-title").value;
+    const categoryId = document.querySelector("#photo-category").value;
+
+    const work = {
+        imageUrl,
+        title,
+        categoryId
+    };
+
+    fetch("http://localhost:5678/api/works", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(work),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            displayModalWorks();
+        });
+}
